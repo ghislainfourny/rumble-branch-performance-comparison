f96d471d1185bc3d81ed4c427b5cbc3cc093a8c8
canberker <canberkerwork@gmail.com>
2019-10-16 23:50:31 +0200

    Remove f strings from the python script for compatibility

--- aggregateResults.py
+++ aggregateResults.py
@@ -17,7 +17,8 @@ if (len(sys.argv) == 5):
 if (len(sys.argv) == 6):
     path = sys.argv[5]
 
-print(f"branch1: {branch1}, branch2: {branch2}, iteration_count: {iteration_count}, dataset:{dataset}, path: {path}")
+# print(f"branch1: {branch1}, branch2: {branch2}, iteration_count: {iteration_count}, dataset:{dataset}, path: {path}")
+print("branch1: " + str(branch1) + ", branch2: " + str(branch2) + " , iteration_count: " + str(iteration_count) + " , dataset: " + str(dataset) + " , path: " + str(path))
 
 branch1_q1_results = []
 branch2_q1_results = []
@@ -37,19 +38,19 @@ with open('./aggregate_results.txt', 'w') as output:
     for infile in listing:
         print("current file is: " + infile)
         with open(path + infile, 'r') as input:
-            if infile.startswith(f"{dataset}-{branch1}-query1-log"):
+            if infile.startswith('{0}-{1}-query1-log'.format(dataset, branch1)):
                 branch1_q1_results.append(int(input.readline()[10:-1]))
-            elif infile.startswith(f"{dataset}-{branch2}-query1-log"):
+            elif infile.startswith('{0}-{1}-query1-log'.format(dataset, branch2)):
                 branch2_q1_results.append(int(input.readline()[10:-1]))
             
-            elif infile.startswith(f"{dataset}-{branch1}-query4-log"):
+            elif infile.startswith('{0}-{1}-query4-log'.format(dataset, branch1)):
                 branch1_q4_results.append(int(input.readline()[10:-1]))
-            elif infile.startswith(f"{dataset}-{branch2}-query4-log"):
+            elif infile.startswith('{0}-{1}-query4-log'.format(dataset, branch2)):
                 branch2_q4_results.append(int(input.readline()[10:-1]))
             
-            elif infile.startswith(f"{dataset}-{branch1}-query5-log"):
+            elif infile.startswith('{0}-{1}-query5-log'.format(dataset, branch1)):
                 branch1_q5_results.append(int(input.readline()[10:-1]))
-            elif infile.startswith(f"{dataset}-{branch2}-query5-log"):
+            elif infile.startswith('{0}-{1}-query5-log'.format(dataset, branch2)):
                 branch2_q5_results.append(int(input.readline()[10:-1]))
 
     branch1_q1_avg = Average(branch1_q1_results)
@@ -62,17 +63,16 @@ with open('./aggregate_results.txt', 'w') as output:
     branch2_q5_avg = Average(branch2_q5_results)
 
     output.writelines([
-        f"{branch1}-Q1: {str(branch1_q1_avg)}\n",
-        f"{branch2}-Q1: {str(branch2_q1_avg)}\n",
-        
-        f"{branch1}-Q4: {str(branch1_q4_avg)}\n",
-        f"{branch2}-Q4: {str(branch2_q4_avg)}\n",
-        
-        f"{branch1}-Q5: {str(branch1_q5_avg)}\n",
-        f"{branch2}-Q5: {str(branch2_q5_avg)}\n",
+        '{0}-Q1: {1}\n'.format(branch1, branch1_q1_avg),
+        '{0}-Q1: {1}\n'.format(branch2, branch2_q1_avg),
+        '{0}-Q4: {1}\n'.format(branch1, branch1_q4_avg),
+        '{0}-Q4: {1}\n'.format(branch2, branch2_q4_avg),
+        '{0}-Q5: {1}\n'.format(branch1, branch1_q5_avg),
+        '{0}-Q5: {1}\n'.format(branch2, branch2_q5_avg),
     ])
 
 if ((branch2_q1_avg > branch1_q1_avg * 11/10)
     or (branch2_q4_avg > branch1_q4_avg * 11/10)
     or (branch2_q5_avg > branch1_q5_avg * 11/10)):
-    sys.exit([f"Performance degredation detected... Please investigate recent merges to {branch2}"])
+    # sys.exit([f"Performance degredation detected... Please investigate recent merges to {branch2}"])
+    sys.exit(["Performance degredation detected... Please investigate recent merges to " + str(branch2)])
